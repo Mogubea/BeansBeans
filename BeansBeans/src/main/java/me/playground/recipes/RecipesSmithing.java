a18362a39c8d6869e0f80d42935fa9e52a01787c
Beandon <90264418+MoguBean@users.noreply.github.com>
2021-12-07 15:25:00 +0000

    Forgot to add the actual RecipesSmithing class..

---
+++ BeansBeans/src/main/java/me/playground/recipes/RecipesSmithing.java
@@ -0,0 +1,33 @@
+package me.playground.recipes;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.RecipeChoice.MaterialChoice;
+import org.bukkit.inventory.SmithingRecipe;
+
+import me.playground.items.BeanItem;
+import me.playground.main.Main;
+
+public class RecipesSmithing {
+	
+	private final Main plugin;
+	private final RecipeManager manager;
+	
+	public RecipesSmithing(Main plugin, RecipeManager manager) {
+		this.plugin = plugin;
+		this.manager = manager;
+		
+		recipe("rose_gold_helmet", BeanItem.ROSE_GOLD_HELMET.getOriginalStack(), new MaterialChoice(Material.GOLDEN_HELMET), new MaterialChoice(Material.COPPER_INGOT), true);
+		recipe("rose_gold_chestplate", BeanItem.ROSE_GOLD_CHESTPLATE.getOriginalStack(), new MaterialChoice(Material.GOLDEN_CHESTPLATE), new MaterialChoice(Material.COPPER_INGOT), true);
+		recipe("rose_gold_leggings", BeanItem.ROSE_GOLD_LEGGINGS.getOriginalStack(), new MaterialChoice(Material.GOLDEN_LEGGINGS), new MaterialChoice(Material.COPPER_INGOT), true);
+		recipe("rose_gold_boots", BeanItem.ROSE_GOLD_BOOTS.getOriginalStack(), new MaterialChoice(Material.GOLDEN_BOOTS), new MaterialChoice(Material.COPPER_INGOT), true);
+	}
+	
+	private SmithingRecipe recipe(String name, ItemStack result, RecipeChoice base, RecipeChoice addition, boolean copyNbt) {
+		SmithingRecipe sr = new SmithingRecipe(plugin.keyRecipe(name), result, base, addition, copyNbt);
+		manager.addRecipe(sr);
+		return sr;
+	}
+	
+}
