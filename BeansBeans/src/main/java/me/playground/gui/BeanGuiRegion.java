419d4caa233a76656a585c7d52f5149e5900b23a
Mogubean <90264418+Mogubean@users.noreply.github.com>
2022-09-22 20:07:57 +0100

    Region GUI can now drop Region Crystal

--- BeansBeans/src/main/java/me/playground/gui/BeanGuiRegion.java
+++ BeansBeans/src/main/java/me/playground/gui/BeanGuiRegion.java
@@ -5,8 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 import me.playground.entity.EntityRegionCrystal;
+import me.playground.items.BeanItem;
+import me.playground.items.lore.Lore;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import net.minecraft.world.entity.Entity;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -33,7 +36,8 @@ public abstract class BeanGuiRegion extends BeanGui {
 			"\u00a77The \u00a79Region Owner\u00a77 and \u00a7bStaff \u00a77are able to modify",
 			"\u00a77various attributes about the \u00a79Region\u00a77 such as;",
 			"\u00a77name, priority, flags and members from this menu.");
-	
+
+	private static final ItemStack removeCrystal = newItem(BeanItem.REGION_CRYSTAL.getItemStack(), Component.text("Drop Region Crystal", NamedTextColor.RED), Lore.getBuilder("Click to drop this &fRegion Crystal&r as an item.").setCompact().setLineLimit(32).build().getLore().get(0));
 	protected final ItemStack rBlank = new ItemStack(Material.BLUE_STAINED_GLASS_PANE);
 	protected final RegionManager rm;
 	protected int regionIdx;
@@ -55,6 +59,7 @@ public abstract class BeanGuiRegion extends BeanGui {
 		this.presetSize = 54;
 
 		updateRegionItems();
+		updateCrystalItem();
 	}
 	
 	protected BeanGuiRegion(Player p, int regionIdx) {
@@ -73,12 +78,15 @@ public abstract class BeanGuiRegion extends BeanGui {
 			openInventory();
 		}
 
-		/*if (e.getRawSlot() == 53) {
+		// Drop crystal
+		else if (e.getRawSlot() == 53) {
 			if (getCrystal() != null) {
 				p.getInventory().addItem(BeanItem.REGION_CRYSTAL.getItemStack()).forEach((idx, item) -> crystal.getBukkitEntity().getWorld().dropItem(crystal.getBukkitEntity().getLocation(), item));
 				crystal.remove(Entity.RemovalReason.DISCARDED);
+				updateCrystalItem();
+				p.playSound(p.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.25F, 0.8F);
 			}
-		}*/
+		}
 	}
 
 	@Override
@@ -120,6 +128,10 @@ public abstract class BeanGuiRegion extends BeanGui {
 
 		rBlank.setType(getRegion().isWorldRegion() ? Material.GREEN_STAINED_GLASS_PANE : Material.BLUE_STAINED_GLASS_PANE);
 		rBlank.editMeta(meta -> meta.displayName(getRegion().getColouredName().decoration(TextDecoration.ITALIC, false)));
+	}
+
+	private void updateCrystalItem() {
+		i.setItem(53, getCrystal() != null ? removeCrystal : rBlank);
 	}
 
 	/**
